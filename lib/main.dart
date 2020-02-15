24220698fb84544fae13e1db7e4d3e140ac72317
Lucas GILHARD <l.gilhard@gmail.com>
2020-02-15 20:46:42 +0100

    add an app bar and a centered image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[50],
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('https://source.unsplash.com/1600x900/?cat'),
+          ),
+        ),
       ),
     ),
   );
